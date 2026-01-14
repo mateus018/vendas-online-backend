18738e227dca55994b247e49db95ce16156b8880
mateus <mateus.nascimento018@gmail.com>
2026-01-14 02:22:53 -0300

    feat: create Post

---
+++ src/user/user.controller.ts
@@ -0,0 +1,11 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import type { CreateUserDto } from './dtos/createUser.dto';
+
+@Controller('user')
+export class UserController {
+    @Post()
+    async createUser(@Body() createUser: CreateUserDto ) {
+        
+       return  {...createUser,password: undefined };
+    }
+}
