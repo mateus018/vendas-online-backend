853112dac10734c83d659b271cc4d38b7af0bdc5
mateus <mateus.nascimento018@gmail.com>
2026-01-18 04:35:22 -0300

    feat: create token JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,15 +3,19 @@ import { compare } from 'bcrypt';
 import { UserEntity } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/user.service';
 import { LoginDto  } from './dtos/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { ReturnLogin } from './dtos/returnLogin.dto';
+import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
+import { LoginPayload } from './dtos/loginPayload.dto';
 
 @Injectable()
 export class AuthService {
 
     constructor(
-        private readonly userService: UserService,
-    ){}
+        private readonly userService: UserService, private jwtService: JwtService
+    )  {}
 
-    async login(loginDto : LoginDto ): Promise<UserEntity> {
+    async login(loginDto : LoginDto ): Promise<ReturnLogin> {
 
         const user: UserEntity | undefined = await this.userService.findUserByEmail(loginDto.email).catch(()=> undefined);
 
@@ -24,7 +28,10 @@ export class AuthService {
             }
         }
 
-        return user;
+        return {
+            accessToken : this.jwtService.sign({...new LoginPayload(user)}),
+            user: new ReturnUserDto(user),
+        };
 
         
 
